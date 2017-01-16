1f7cf62ea09239952d9f6dfba7dc4e1fc3f903aa
AbiramiE <abirami2401@gmail.com>
2017-01-16 11:31:20 +0530

    Initial commit

---
+++ app/src/main/java/journaldev/com/spinners/Sampl1.java
@@ -0,0 +1,13 @@
+package journaldev.com.spinners;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sampl1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sampl1);
+    }
+}
